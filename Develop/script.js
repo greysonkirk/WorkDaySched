5ba8796cec477a623db22a4bbcccdc1cf8b5416f
Greyson Kirk <greysonkirk@yahoo.com>
2020-11-07 18:08:05 -0600

    made planner code changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,12 @@
-let today = moment().format("MMM Do YY");
+let today = moment().format("MMMM Do YYYY");
 
 $("#currentDay").text(today)
 
 console.log(today)
-
-hours.forEach(hour => {
-    let $planner = $(`
-    <div class="container align-center">
-    <div class="row">
-        <div id="hour" class="col-md-2 hour">${hour.hourText} </div>
-        <div id="task" class="col-md-9 description p-0">planner </div>
-        <div id="Btn" class="col-md-1 saveBtn">save </div>
-    </div>
-    </div>
-    `);
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
 let hours = [{
         id: "0",
         hourText: "9 am",
-        time: "9",
+        time: "09",
         task: ""
     },
     {
@@ -57,32 +29,65 @@ let hours = [{
     },
     {
         id: "4",
-        hourText: "01 pm",
+        hourText: "1 pm",
         time: "13",
         task: ""
     },
     {
         id: "5",
-        hourText: "02 pm",
+        hourText: "2 pm",
         time: "14",
         task: ""
     },
     {
         id: "6",
-        hourText: "03 pm",
+        hourText: "3 pm",
         time: "15",
         task: ""
     },
     {
         id: "7",
-        hourText: "04 pm",
+        hourText: "4 pm",
         time: "16",
         task: ""
     },
     {
         id: "8",
-        hourText: "05 pm",
+        hourText: "5 pm",
         time: "17",
         task: ""
     },
 ]
+
+function loadDayPlanner() {
+
+    for (let i = 0; i < hours.length; i++) {
+        const hour = hours[i];
+        //adding class based off time.
+        let txtClass = ''
+        if (hour.time < moment().format("HH") - 4) { txtClass = "past"; } else if (parseInt(hour.time) === moment().format("HH") - 4) { txtClass = "present"; } else if (hour.time > moment().format("HH") - 4) { txtClass = "future" }
+
+
+
+        //adding each time slot 
+        let hourID = hour.id
+        let $planner = $(`
+      <form class="row">
+        <div id="hour" class="col-md-2 hour">${hour.hourText} </div>
+        <div id="task" class="col-md-9 description p-0"><textarea id="${hourID}" class="${txtClass}"></textarea></div>
+        <button id="Btn" class="col-md-1 saveBtn"><i class="far fa-save fa-lg  "></i> </button>
+    </form>
+    `);
+
+        console.log(hour.time)
+        console.log(moment().format("HH") - 4)
+
+        $("#container").append($planner);
+
+    };
+
+
+};
+
+
+loadDayPlanner()
